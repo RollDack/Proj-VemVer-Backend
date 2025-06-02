213c9d513ebbe19510f7f84911a42629c1c8b8c1
RollDack <marquesmatheus.psn@gmail.com>
2025-06-02 13:36:42 -0300

    Ajustes de rotas e models

--- api/funcionario/model_funcionario.py
+++ api/funcionario/model_funcionario.py
@@ -5,6 +5,7 @@ class Funcionario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cpf = db.Column(db.String(11), unique=True, nullable=False)
     nome = db.Column(db.String(100), nullable=False)
+    senha = db.Column(db.String(50), nullable=False)
     idade = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     cep = db.Column(db.String(8), nullable=False)
